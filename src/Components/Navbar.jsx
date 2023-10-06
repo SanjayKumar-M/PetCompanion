07355413ce35b83f1ac58e7e9d0303a50fe12d56
SanjayKumar-M <sanjaymoad786@gmail.com>
2023-10-06 23:49:39 +0530

    contact

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,18 +5,18 @@ import { FaUser, FaPaw, FaSearch } from 'react-icons/fa';
 import Link from 'next/link';
 
 const Navbar = () => {
-    const navItems = ["Home", "About", "Contact Us"];
+    // const navItems = ["Home", "About", "Contact Us"];
     return (
         <nav className='flex items-center  text-center text-2xl p-4 justify-around bg-gradient-to-r from-indigo-200 via-red-200 to-yellow-100'>
             <a href='/' className='text-black font-extrabold p-2 text-3xl font-mono inline-flex gap-2'>
                 <span><FaPaw /></span>PetCompanion
             </a>
-            <ul className='hidden lg:flex items-center gap-10 cursor-pointer'>
+            <ul className='lg:flex items-center gap-10 cursor-pointer'>
                 <li className='hidden lg:flex items-center gap-2 cursor-pointer font-mono font-bold'>
                     <Link href='/'>Home</Link>
                 </li>
                 <li className='hidden lg:flex items-center gap-10 cursor-pointer font-mono font-bold'>
-                <Link href='/about'>About</Link>
+                    <Link href='/about'>About</Link>
                 </li>
                 <li className='hidden lg:flex items-center gap-2 cursor-pointer font-mono font-bold'>
                     <Link href='/contacts'>Contact</Link>
--- src/app/contacts/page.jsx
+++ src/app/contacts/page.jsx
@@ -1,15 +1,37 @@
-"use client"
+"use client";
 
-import Navbar from '@/Components/Navbar'
-import React from 'react'
+import Navbar from '@/Components/Navbar';
+import React from 'react';
 
 const Contacts = () => {
     return (
+        <>
+        <Navbar />
+            <div className="min-h-screen flex flex-col items-center justify-center p-3 ">
 
-        <div>
-            <Navbar />
-            Contacts</div>
-    )
-}
+                <div className="w-full max-w-md p-6 border border-black rounded-xlbg-gradient-to-b from-gray-900 to-gray-600 bg-gradient-to-r text-white text-3xl ">
+                    <p className="text-center mb-4">Want to know more? Reach out to us</p>
+                    <form action="" method="post" className="flex flex-col space-y-4">
+                        <label className="text-sm">Full Name</label>
+                        <input type="text" className="border border-black py-2 px-3" />
 
-export default Contacts
+                        <label className="text-sm">Email</label>
+                        <input type="email" className="border border-black py-2 px-3" />
+
+                        <label className="text-sm">You are</label>
+                        <input type="text" className="border border-black py-2 px-3" />
+
+                        <label className="text-sm">Message</label>
+                        <textarea className="border border-black py-2 px-3 h-24"></textarea>
+
+                        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                            Submit
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Contacts;
