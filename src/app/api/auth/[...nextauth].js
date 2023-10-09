476851de84ef06920c5cb4dbcc90f3191ae07da2
SanjayKumar-M <sanjaymoad786@gmail.com>
2023-10-09 20:11:10 +0530

    setting auth

--- src/app/api/auth/[...nextauth].js
+++ src/app/api/auth/[...nextauth].js
@@ -2,8 +2,18 @@ import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 
 export default NextAuth({
-    
 
+    providers: [
+        GoogleProvider({
+          clientId: process.env.GOOGLE_CLIENT_ID,
+          clientSecret: process.env.GOOGLE_CLIENT_SECRET
+        })
+      ]
+    
+ 
 })
 
+const handler = NextAuth(authOptions);
 export {handler as GET, handler as POST}
+
+
