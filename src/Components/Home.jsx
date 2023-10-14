f2841a26021e286af3588f83a7591483c73f39ad
SanjayKumar-M <sanjaymoad786@gmail.com>
2023-10-14 23:22:30 +0530

    home page

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
-
-
-
+import Image from 'next/image';
+import homedog from '../assets/homedog.jpg'
+import family from '../assets/family.png'
+import '../../src/app/globals.css'
 const Home = () => {
+
     return (
-       <div className=''>
-        Hello Home
-       </div>
+
+        <div className=''>
+            <div>
+                Welcome to PetCompanion Where Every Tail Wags and Every Heart Purrs!
+            </div>
+            <div>
+                <Image src={family} width={96} height={96} className='h-96 w-96 ' />
+            </div>
+
+        </div>
+
     );
+
 };
 
 export default Home;
--- src/app/contacts/page.jsx
+++ src/app/contacts/page.jsx
@@ -8,17 +8,17 @@ import Image from 'next/image';
 const Contacts = () => {
     return (
         < div className=''>
-        <Navbar />
-        <div className="min-h-screen flex flex-col-reverse items-center justify-around p-3 md:flex-row-reverse">
+            <Navbar />
+            <div className="min-h-screen flex flex-col-reverse items-center justify-around p-3 md:flex-row-reverse">
 
-        <div className="w-full max-w-md p-6 rounded-3xl shadow-2xl shadow-gray-950 ">
+                <div className="w-full max-w-md p-6 rounded-3xl shadow-2xl shadow-gray-950 ">
                     <p className="text-center mb-4 font-extrabold text-lg">Bark at us anytime! Your pets voices matter</p>
                     <form action="" method="post" className="flex flex-col space-y-4 ">
                         <label className="text-sm" >Full Name</label>
-                        <input type="text" className="border border-black py-2 px-3 rounded-3xl" placeholder='Elon Musk'/>
+                        <input type="text" className="border border-black py-2 px-3 rounded-3xl" placeholder='Elon Musk' />
 
                         <label className="text-sm">Email</label>
-                        <input type="email" className="border border-black py-2 px-3 rounded-3xl" placeholder='example@email.com'/>
+                        <input type="email" className="border border-black py-2 px-3 rounded-3xl" placeholder='example@email.com' />
 
                         <label className="text-sm">You are</label>
                         <input type="text" className="border border-black py-2 px-3 rounded-3xl" placeholder='Buyer / Seller' />
@@ -33,11 +33,11 @@ const Contacts = () => {
                 </div>
 
                 <div className='flex flex-col items-center justify-center'>
-                <h1 className='font-bold text-center text-6xl transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 '>In a world full of chatter, we're<br /> here to listen to your pets<br /><span className='bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 text-transparent bg-clip-text'> meows and woofs</span></h1>
-                <Image src={contact} height={200} width={200} className="object-contain h-96 w-96 rounded-3xl mt-7 shadow-xl shadow-orange-400 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300" />
+                    <h1 className='font-bold text-center text-6xl transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 '>In a world full of chatter, we're<br /> here to listen to your pets<br /><span className='bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 text-transparent bg-clip-text'> meows and woofs</span></h1>
+                    <Image src={contact} height={200} width={200} className="object-contain h-96 w-96 rounded-3xl mt-7 shadow-xl shadow-orange-400 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300" />
 
 
-                   
+
                 </div>
             </div>
         </div>
